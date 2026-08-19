7907f7448e10cc476312a29cd757f89acb96d2c6
mytysoldier <yoshi008t@gmail.com>
2026-08-19 21:43:48 +0900

    セッションBindingの分岐意図を補足

--- RaceFuelTimer/App/RaceFuelTimerApp.swift
+++ RaceFuelTimer/App/RaceFuelTimerApp.swift
@@ -7,6 +7,7 @@ struct RaceFuelTimerApp: App {
     var body: some Scene {
         WindowGroup {
             NavigationStack {
+                // Optional の Session を、画面が更新できる Binding<Session> として安全に取り出す。
                 if let sessionBinding = Binding($session) {
                     SessionTimerView(session: sessionBinding) {
                         session = nil
